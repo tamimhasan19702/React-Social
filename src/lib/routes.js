8260f1d45ca10bb3739c4b37bd77d9244e278a94
tamimhasan19702 <tamimhasancu@gmail.com>
2023-02-28 02:21:35 +0600

    Protected route setup done

--- src/lib/routes.js
+++ src/lib/routes.js
@@ -1,14 +1,20 @@
 import {createBrowserRouter} from 'react-router-dom';
 import Login from '../components/auth/Login';
 import Register from '../components/auth/Register';
+import Dashboard from '../components/Dashboard/Dashboard';
 
 
 export const ROOT = "/";
 export const LOGIN = "/login";
 export const REGISTER = "/register";
+export const PROTECTED = "/protected";
+export const DESHBOARD = "/protected/dashboard";
 
 export const router = createBrowserRouter([
  {path: ROOT, element: 'Public Root'},
  {path: LOGIN, element: <Login />},
  {path: REGISTER, element: <Register />},
+ {path: PROTECTED, element: "", children: [
+    {}
+ ]},
 ])
