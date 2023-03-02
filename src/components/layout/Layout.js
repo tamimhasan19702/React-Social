c4e9a51965dac952f06a457438bd753a903a1dcf
tamimhasan19702 <tamimhasancu@gmail.com>
2023-03-03 01:52:50 +0600

    dashboard route error fixed

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+function Layout() {
+  return (
+    <>
+    this is good
+    <Outlet/>
+    </>
+  )
+}
+
+export default Layout
--- src/lib/routes.js
+++ src/lib/routes.js
@@ -2,22 +2,24 @@ import {createBrowserRouter} from 'react-router-dom';
 import Login from '../components/auth/Login';
 import Register from '../components/auth/Register';
 import Dashboard from '../components/Dashboard/Dashboard';
+import Layout from '../components/layout/Layout';
 
 
 export const ROOT = "/";
 export const LOGIN = "/login";
 export const REGISTER = "/register";
 export const PROTECTED = "/protected";
-export const DESHBOARD = "/protected/dashboard";
+export const DASHBOARD = "/protected/dashboard";
 
 export const router = createBrowserRouter([
  {path: ROOT, element: 'Public Root'},
  {path: LOGIN, element: <Login />},
  {path: REGISTER, element: <Register />},
- {path: PROTECTED, element: "this is a string", children: [
-    {
-      path: Dashboard,
-      element: 'Dashboard',
-   }
- ]},
+ {path: PROTECTED, element: <Layout/>, 
+ children: [{
+      path: DASHBOARD,
+      element: "dashboard",
+   },
+ ],
+},
 ])
