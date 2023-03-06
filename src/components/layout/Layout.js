be6dacadd87b092a2ed41a68e616c4e26e9269b8
tamimhasan19702 <tamimhasancu@gmail.com>
2023-03-06 20:43:47 +0600

    protected page to login page redirection done

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { LOGIN } from '../../lib/routes';
 
 function Layout() {
+  const {pathname} = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+  if(pathname.startsWith("/protected")){
+    navigate(LOGIN)
+  }
+  },[pathname])
+
   return (
     <>
-    this is good
+    this is good: 
     <Outlet/>
     </>
   )
