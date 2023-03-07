04e21ab576b9bb05c5fe38436d2c5e7fb8d1460d
tamimhasan19702 <tamimhasancu@gmail.com>
2023-03-07 14:32:31 +0600

    protected route specified with user state

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,16 +1,20 @@
 import React, { useEffect } from 'react'
 import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import { LOGIN } from '../../lib/routes';
+import { useAuth } from '../../hooks/auth';
 
 function Layout() {
   const {pathname} = useLocation();
   const navigate = useNavigate();
+  const {user,isLoading} = useAuth();
 
   useEffect(() => {
-  if(pathname.startsWith("/protected")){
+  if(pathname.startsWith("/protected") && !user){
     navigate(LOGIN)
   }
   },[pathname])
+
+  if(isLoading) return "Loading ..."
 
   return (
     <>
