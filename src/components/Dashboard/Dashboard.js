a0e2a470bf5c7f8d099fc9deaa8e467ab554acd1
tamimhasan19702 <tamimhasancu@gmail.com>
2023-03-24 03:17:01 +0600

    text auto size jquery package added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,14 +1,17 @@
 import { Heading, HStack } from "@chakra-ui/layout"
-import { Box,Button } from "@chakra-ui/react"
+import { Box,Button,Textarea } from "@chakra-ui/react"
+import TextareaAutosize from "react-textarea-autosize"
 
 function Dashboard() {
   return (
     <Box maxW="600px" mx="auto" py="10">
      <form>
+      
       <HStack justify="space-between">
         <Heading size="lg">New Post</Heading>
         <Button colorScheme="teal" type="submit">Post</Button>
       </HStack>
+     <Textarea as={TextareaAutosize} resize="none" mt="5" placeholder="Write something new... "/>
      </form>
     </Box>
   )
