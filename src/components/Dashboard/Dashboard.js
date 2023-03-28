e0285a38a28d8da09415c25118f64d70f2dac4eb
tamimhasan19702 <tamimhasancu@gmail.com>
2023-03-28 15:57:42 +0600

    dashboard post taking my value

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,17 +1,34 @@
 import { Heading, HStack } from "@chakra-ui/layout"
 import { Box,Button,Textarea } from "@chakra-ui/react"
 import TextareaAutosize from "react-textarea-autosize"
+import {useForm} from "react-hook-form";
 
 function Dashboard() {
+ 
+  const {register,handleSubmit,reset} = useForm();
+
+  function handlePost(data){
+    console.log(data);
+    reset();
+  }
+
   return (
     <Box maxW="600px" mx="auto" py="10">
-     <form>
+     <form onSubmit={handleSubmit(handlePost)}>
       
       <HStack justify="space-between">
         <Heading size="lg">New Post</Heading>
         <Button colorScheme="teal" type="submit">Post</Button>
       </HStack>
-     <Textarea as={TextareaAutosize} resize="none" mt="5" placeholder="Write something new... "/>
+     <Textarea 
+     as={TextareaAutosize} 
+     resize="none" 
+     mt="5" 
+     placeholder="Write something new... "
+     minRows={3}
+     {...register("text", {required: true })}
+     />
+
      </form>
     </Box>
   )
