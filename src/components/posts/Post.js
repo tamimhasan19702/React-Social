82699eab01b58300b8d9b0ea59d536b6afd741c1
tamimhasan19702 <tamimhasancu@gmail.com>
2023-03-29 00:37:21 +0600

    update on new post component and list

---
+++ src/components/posts/Post.js
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+function Post() {
+  return (
+    <Box p="2" maxW="600px"></Box>
+  )
+}
+
+export default Post;
---
+++ src/components/posts/PostList.js
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+export default function PostList({posts}) {
+  return (
+    <Box px="4">
+
+    </Box>
+  );
+}
