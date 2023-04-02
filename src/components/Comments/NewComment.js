9507816d88466d6768979623aeba634b0161913a
tamimhasan19702 <tamimhasancu@gmail.com>
2023-04-02 22:43:50 +0600

    user icon added to the  comment button

--- src/components/Comments/NewComment.js
+++ src/components/Comments/NewComment.js
@@ -1,10 +1,18 @@
 import { Box, Button, Flex, Input } from "@chakra-ui/react";
+import { useAuth } from "../../hooks/useAuth";
+import Avatar from "../profile/Avatar";
 
 export default function NewComment() {
+
+  const {user,isLoading} = useAuth();
+  
+  if(isLoading) return "Loading...";
+
   return (
    <Box maxW="600px" mx="auto" py="6">
     
     <Flex padding="4">
+      <Avatar user={user}/>
      <Box flex="1" ml="4">
         <form onSubmit={() => {}}>
           <Box>
