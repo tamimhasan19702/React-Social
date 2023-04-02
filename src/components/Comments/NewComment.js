07dfe3f4b9163daeb7641cd2f4d9685ca2c3de79
tamimhasan19702 <tamimhasancu@gmail.com>
2023-04-02 23:22:22 +0600

    newComment component updates

--- src/components/Comments/NewComment.js
+++ src/components/Comments/NewComment.js
@@ -1,31 +1,40 @@
 import { Box, Button, Flex, Input } from "@chakra-ui/react";
 import { useAuth } from "../../hooks/useAuth";
 import Avatar from "../profile/Avatar";
+import {useForm} from 'react-hook-form';
+
 
 export default function NewComment() {
-
   const {user,isLoading} = useAuth();
-  
+  const {register,handleSubmit,reset} = useForm();
+
+  function handleAddComment(){
+   
+    reset()
+  }
+
   if(isLoading) return "Loading...";
 
   return (
    <Box maxW="600px" mx="auto" py="6">
     
     <Flex padding="4">
-      <Avatar user={user}/>
+      <Avatar user={user} size="sm"/>
      <Box flex="1" ml="4">
-        <form onSubmit={() => {}}>
+        <form onSubmit={handleSubmit(handleAddComment)}>
           <Box>
             <Input 
             size="sm"
             variant="flushed"
-            placeholder="write Comment"
-            autoComplete="off"/>
+            placeholder="write Comment..."
+            autoComplete="off"
+            {...register('text',{required: true})}
+            />
           </Box>
           
           <Flex pt="2">
            <Button
-        //    isLoading={}
+        //isLoading={}
         type="submit"
         colorScheme="teal"
         ml="auto"
