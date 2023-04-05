b7d9c28d2acacaf423ffd6797740a0128801a066
tamimhasan19702 <tamimhasancu@gmail.com>
2023-04-05 21:22:57 +0600

    comment ascending index added and created

---
+++ src/components/Comments/singleComment.js
@@ -0,0 +1,11 @@
+import { Box, Flex } from "@chakra-ui/react";
+
+export default function singleComment() {
+  return (
+    <Box px="4" py="2" maxW="600px" mx="auto" textAlign="left">
+     <Flex pb="2">
+        user avatar here
+     </Flex>
+    </Box>
+  )
+}
--- src/hooks/useCommentsList.js
+++ src/hooks/useCommentsList.js
@@ -1,12 +1,12 @@
 
-import { query,collection,where} from "firebase/firestore";
+import { query,collection,where,orderBy} from "firebase/firestore";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { db } from "../lib/firebase";
 
 export default function useCommentsList(postId) {
   
   
-    const q = query(collection(db,"comments"), where("postId","==",postId));
+    const q = query(collection(db,"comments"), where("postId","==",postId),orderBy("date","asc"));
     const [comments,isLoading,error] = useCollectionData(q);
 
     if(error) throw error;
