4153fac60df379281e099f38460c0cc92637fd25
tamimhasan19702 <tamimhasancu@gmail.com>
2023-04-08 00:45:47 +0600

    query index ascending added to useCommentList hook

--- src/components/Comments/CommentList.js
+++ src/components/Comments/CommentList.js
@@ -1,5 +1,15 @@
-import React from "react";
+import { Box } from "@chakra-ui/react";
+import useCommentList from "../../hooks/useCommentList";
 
-export default function CommentList() {
-  return <div>CommentList</div>;
+export default function CommentList({post}) {
+    const {id} = post;
+    const {comments,isLoading} = useCommentList(id);
+    
+    if (isLoading) return "Loading..."
+  
+    return(
+        <Box>
+        {comments.map((comment) =><>{comment.text}</>)}
+        </Box>
+    );
 }
--- src/hooks/useCommentList.js
+++ src/hooks/useCommentList.js
@@ -1,9 +1,12 @@
-import { query,collection,where} from "firebase/firestore"
+import { query,collection,where,orderBy} from "firebase/firestore"
 import { db } from "../lib/firebase"
 import { useCollectionData } from "react-firebase-hooks/firestore";
 
 export default function useCommentList(postId) {
-   const q = query(collection(db,"comments"),where("postId"), "==", postId);
+   const q = query(
+    collection(db,"comments"),
+    where("postId", "==", postId),
+    orderBy("date","asc"));
 
    const [comments,isLoading,error] = useCollectionData(q)
   
