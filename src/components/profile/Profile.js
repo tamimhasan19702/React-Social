3943d642b09d24b3cd2e367d9b750b012f6cfc09
tamimhasan19702 <tamimhasancu@gmail.com>
2023-04-10 21:23:25 +0600

    usePostlist hook fixed for specific users post

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,10 +1,15 @@
-import { Flex, HStack, Stack,Text } from "@chakra-ui/react";
+import { Divider, Flex, HStack, Stack,Text } from "@chakra-ui/react";
 import React from "react";
 import Avatar from "./Avatar";
+import { useParams } from "react-router-dom";
+import PostList from "../posts/PostList";
+import usePosts from "../../hooks/usePosts";
 
 export default function Profile() {
+   const {id} = useParams();
+   const {posts,isLoading:postLoading} = usePosts(id);
   
-  
+
     return (
         <Stack spacing="5">
           <Flex p={["4","6"]} pos="relative" align="center"> 
@@ -13,13 +18,29 @@ export default function Profile() {
            @username here
            
            <HStack spacing="10">
-            <Text color="gray.100">
-
+            <Text color="gray.700" fontSize={["sm","lg"]}>
+            Posts: 10
+            </Text>
+            <Text color="gray.700" fontSize={["sm","lg"]}>
+            Likes: todo!
+            </Text>
+            <Text color="gray.700" fontSize={["sm","lg"]}>
+            joined: jan 01 2022
             </Text>
            </HStack>
           
           </Stack>
           </Flex>
+          <Divider />
+
+         {
+            postLoading ? (
+            <Text>Posts are loading...</Text>
+            ): (
+            <PostList posts={posts}/>
+            )
+         }
+         
         </Stack>
     );
 }
