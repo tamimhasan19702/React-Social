c5690602ff6a228a2ede2ea69a59b653275ec9d9
tamimhasan19702 <tamimhasancu@gmail.com>
2023-04-11 21:51:51 +0600

    Profile component updates and edit profile component updates

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,35 +1,52 @@
-import { Divider, Flex, HStack, Stack,Text } from "@chakra-ui/react";
+import { Button, Divider, Flex, HStack, Stack,Text, useDisclosure } from "@chakra-ui/react";
 import React from "react";
 import Avatar from "./Avatar";
 import { useParams } from "react-router-dom";
 import PostList from "../posts/PostList";
 import usePosts from "../../hooks/usePosts";
+import useUsers from "../../hooks/useUsers";
+import {format} from "date-fns";
+import EditProfile from "./EditProfile";
+
 
 export default function Profile() {
    const {id} = useParams();
    const {posts,isLoading:postLoading} = usePosts(id);
-  
+   const {user,isLoading:userLoading} = useUsers(id);
+   const {isOpen,onOpen,onClose} = useDisclosure()
+
+   if(userLoading) return "Loading...";
 
     return (
         <Stack spacing="5">
           <Flex p={["4","6"]} pos="relative" align="center"> 
-          <Avatar />
+
+          <Avatar user={user} size="2xl"/>
+          <Button 
+          pos="absolute" 
+          mb="2" top="6" 
+          right="6" 
+          colorScheme="teal"
+          onClick={onOpen}>
+            Change avatar
+            </Button>
           <Stack ml="10">
-           @username here
            
+           @<Text fontSize="2xl" fontWeight="bold">{user.username}</Text>
            <HStack spacing="10">
             <Text color="gray.700" fontSize={["sm","lg"]}>
-            Posts: 10
+            Posts: {posts.length}
             </Text>
             <Text color="gray.700" fontSize={["sm","lg"]}>
             Likes: todo!
             </Text>
             <Text color="gray.700" fontSize={["sm","lg"]}>
-            joined: jan 01 2022
+            joined: {format(user.date,"MMM YYY")}
             </Text>
            </HStack>
           
           </Stack>
+          <EditProfile isOpen={isOpen}/>
           </Flex>
           <Divider />
 
@@ -40,7 +57,7 @@ export default function Profile() {
             <PostList posts={posts}/>
             )
          }
-         
+
         </Stack>
     );
 }
